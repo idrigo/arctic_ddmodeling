766ff45a55297cb8a26fb10494060f4d44784b7b
Ilya <ilya.drigo@gmail.com>
2019-05-14 18:58:38 +0300

    pool.starmap implemented

--- spatial.py
+++ spatial.py
@@ -4,8 +4,10 @@ import warnings
 
 warnings.filterwarnings('ignore')
 
+import multiprocessing
 from multiprocessing import Pool
-
+from timeit import default_timer as timer
+from itertools import product
 
 from sklearn.linear_model import Lasso
 from src.feature_table import FeatureTable
@@ -13,24 +15,14 @@ from src.dataset import load_features, load, load_variable_years
 from src.models import regress
 
 
-# configuration
-global cfg
-cfg = dict(years_train=list(range(2010, 2012)),
-           years_test=[2014, 2015],
-           X_vars=['ice_conc', 'tair'],
-           y_var='thick_cr2smos',
-           ft=FeatureTable(dx=2, dy=2, dt=2),
-           model=Lasso(alpha=0.1, max_iter=10000))
 
-box = [0, 452, 0, 406]  # x x y y boundaries
+'''
+    for i in tqdm(range(box[0], box[1], 1)):
+        for j in tqdm(range(box[2], box[3], 1)):
+            point = (i, j)
 
-dims = np.shape(load_variable_years(cfg['y_var'], cfg['years_test']))
-out = np.empty(shape=dims)
-out[:] = np.nan
-
-y_arr_train, X_arr_train = load_features(cfg['y_var'], cfg['X_vars'], cfg['years_train'])
-y_arr_test, X_arr_test = load_features(cfg['y_var'], cfg['X_vars'], cfg['years_test'])
-
+            out[:, i, j] = predict(point)
+'''
 
 def predict(point):
     y_train = y_arr_train[:, point[0], point[1]]
@@ -51,19 +43,54 @@ def predict(point):
     if np.count_nonzero(~np.isnan(y_train)) == 0:
         pred = np.empty_like(y_train)
         pred[:] = np.nan
-        tqdm.write('passing point')
     else:
         mse_val, pred = regress(X_train, y_train, X_test, y_test, model=cfg['model'])
 
     return pred
 
 
-for i in tqdm(range(box[0], box[1], 1)):
-    for j in tqdm(range(box[2], box[3], 1)):
-        point = (i, j)
+def parallel_process(i, j):
+    #t, i, j = np.unravel_index(i, dims)
+    point = [i, j]
 
-        out[:, i, j] = predict(point)
+    return predict(point)
 
-np.save('data/out.npy', out )
+def idx(i,j):
+    return [i,j]
+if __name__ == '__main__':
+    # configuration
+    global cfg
+    cfg = dict(years_train=list(range(2010, 2012)),
+               years_test=[2014, 2015],
+               X_vars=['ice_conc', 'tair'],
+               y_var='thick_cr2smos',
+               ft=FeatureTable(dx=2, dy=2, dt=2),
+               model=Lasso(alpha=0.1, max_iter=10000))
+
+    dims = np.shape(load_variable_years(cfg['y_var'], cfg['years_test']))
+
+    out = np.empty(shape=dims)
+    out[:] = np.nan
+
+    y_arr_train, X_arr_train = load_features(cfg['y_var'], cfg['X_vars'], cfg['years_train'])
+    y_arr_test, X_arr_test = load_features(cfg['y_var'], cfg['X_vars'], cfg['years_test'])
+
+    #box=[200,250,150,200]
+    #i = range(box[0], box[1], 1)
+    #j = range(box[2], box[3], 1)
+
+    i = range(dims[1])
+    j = range(dims[2])
+
+    out_flat = list(product(i, j))
+
+    start = timer()
+    with Pool(64) as pool:
+        res = pool.starmap(parallel_process, out_flat, 1)
+        #idx = pool.starmap(idx, out_flat)
+    np.save('res.npy', res)
+    #np.save('idx.npy', idx)
+    end = timer()
+    print('Time = ', end-start)
 
 
